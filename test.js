3d33099042edb703e438659b4ad0b0073c5489bb
Stu Thompson <stuart@stuartthompson.net>
2021-08-02 09:58:02 -0700

    JavaScript Sample

---
+++ test.js
@@ -0,0 +1,15 @@
+function addNumbers(a, b) {
+    return a + b;
+}
+function isMagicKeyword(word) {
+    if (word === 'magic') {
+        return true;
+    }
+    return false;
+}
+const result = addNumbers(1, 2);
+let isMagic = isMagicKeyword('magic');
+console.log(`${isMagic}, ${result}`);
+
+
+
