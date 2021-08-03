ca5387fe05b52aec7a34f256ddaa2c36b83c9899
Stu Thompson <stuart@stuartthompson.net>
2021-08-03 09:25:20 -0700

    Java Sample: Adds Java code sample.

---
+++ samples/Java.java
@@ -0,0 +1,12 @@
+public class HelloWorld {
+    public static void main(
+        String[] args
+    ) {
+        int num = 123;
+        String msg = "Hello, ";
+        if (num > 0) {
+            msg += " World!";
+        } 
+        System.out.println(msg);
+    }
+}
